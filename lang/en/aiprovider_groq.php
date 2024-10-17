368371659df6ea421a7f515d7aa4c54874758de0
Marcus Green <marcusavgreen@gmail.com>
2024-10-17 21:10:45 +0100

    First commit

---
+++ lang/en/aiprovider_groq.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component aiprovider_groq, language 'en'.
+ *
+ * @package    aiprovider_groq
+ * @copyright  2024 Marcus Green
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['action:generate_image:endpoint'] = 'API endpoint';
+$string['action:generate_image:model'] = 'AI model';
+$string['action:generate_image:model_desc'] = 'The model used to generate images from user prompts.';
+$string['action:generate_text:endpoint'] = 'API endpoint';
+$string['action:generate_text:model'] = 'AI model';
+$string['action:generate_text:model_desc'] = 'The model used to generate the text response.';
+$string['action:generate_text:systeminstruction'] = 'System instruction';
+$string['action:generate_text:systeminstruction_desc'] = 'This instruction is sent to the AI model along with the user\'s prompt. Editing this instruction is not recommended unless absolutely required.';
+$string['action:summarise_text:endpoint'] = 'API endpoint';
+$string['action:summarise_text:model'] = 'AI model';
+$string['action:summarise_text:model_desc'] = 'The model used to summarise the provided text.';
+$string['action:summarise_text:systeminstruction'] = 'System instruction';
+$string['action:summarise_text:systeminstruction_desc'] = 'This instruction is sent to the AI model along with the user\'s prompt. Editing this instruction is not recommended unless absolutely required.';
+$string['apikey'] = 'Groq API key';
+$string['apikey_desc'] = 'Get a key from your <a href="https://console.groq.com/keys" target="_blank">Groq API keys</a>.';
+$string['enableglobalratelimit'] = 'Set site-wide rate limit';
+$string['enableglobalratelimit_desc'] = 'Limit the number of requests that the groq API provider can receive across the entire site every hour.';
+$string['enableuserratelimit'] = 'Set user rate limit';
+$string['enableuserratelimit_desc'] = 'Limit the number of requests each user can make to the groq API provider every hour.';
+$string['globalratelimit'] = 'Maximum number of site-wide requests';
+$string['globalratelimit_desc'] = 'The number of site-wide requests allowed per hour.';
+$string['orgid'] = 'Groq organization ID';
+$string['orgid_desc'] = 'Get your Groq organization ID from your <a href="https://console.groq.com/settings" target="_blank">groq account</a>.';
+$string['pluginname'] = 'Groq API Provider';
+$string['privacy:metadata'] = 'The Groq API provider plugin does not store any personal data.';
+$string['privacy:metadata:aiprovider_groq:externalpurpose'] = 'This information is sent to the groq API in order for a response to be generated. Your groq account settings may change how groq stores and retains this data. No user data is explicitly sent to groq or stored in Moodle LMS by this plugin.';
+$string['privacy:metadata:aiprovider_groq:model'] = 'The model used to generate the response.';
+$string['privacy:metadata:aiprovider_groq:numberimages'] = 'The number of images used in the response. When generating images.';
+$string['privacy:metadata:aiprovider_groq:prompttext'] = 'The user entered text prompt used to generate the response.';
+$string['privacy:metadata:aiprovider_groq:responseformat'] = 'The format of the response. When generating images.';
+$string['userratelimit'] = 'Maximum number of requests per user';
+$string['userratelimit_desc'] = 'The number of requests allowed per hour, per user.';
---
+++ settings.php
@@ -0,0 +1,98 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin administration pages are defined here.
+ *
+ * @package ke    aiprovider_groq
+ * @copyright   2024 Marcus Green
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use core_ai\admin\admin_settingspage_provider;
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($hassiteconfig) {
+    // Provider specific settings heading.
+    $settings = new admin_settingspage_provider(
+        'aiprovider_groq',
+        new lang_string('pluginname', 'aiprovider_groq'),
+        'moodle/site:config',
+        true,
+    );
+
+    $settings->add(new admin_setting_heading(
+        'aiprovider_groq/general',
+        new lang_string('settings', 'core'),
+        '',
+    ));
+
+    // Setting to store Groq API key.
+    $settings->add(new admin_setting_configpasswordunmask(
+        'aiprovider_groq/apikey',
+        new lang_string('apikey', 'aiprovider_groq'),
+        new lang_string('apikey_desc', 'aiprovider_groq'),
+        '',
+    ));
+
+    // Setting to store Groq organization ID.
+    $settings->add(new admin_setting_configtext(
+        'aiprovider_groq/orgid',
+        new lang_string('orgid', 'aiprovider_groq'),
+        new lang_string('orgid_desc', 'aiprovider_groq'),
+        '',
+        PARAM_TEXT,
+    ));
+
+    // Setting to enable/disable global rate limiting.
+    $settings->add(new admin_setting_configcheckbox(
+        'aiprovider_groq/enableglobalratelimit',
+        new lang_string('enableglobalratelimit', 'aiprovider_groq'),
+        new lang_string('enableglobalratelimit_desc', 'aiprovider_groq'),
+        0,
+    ));
+
+    // Setting to set how many requests per hour are allowed for the global rate limit.
+    // Should only be enabled when global rate limiting is enabled.
+    $settings->add(new admin_setting_configtext(
+        'aiprovider_groq/globalratelimit',
+        new lang_string('globalratelimit', 'aiprovider_groq'),
+        new lang_string('globalratelimit_desc', 'aiprovider_groq'),
+        100,
+        PARAM_INT,
+    ));
+    $settings->hide_if('aiprovider_groq/globalratelimit', 'aiprovider_groq/enableglobalratelimit', 'eq', 0);
+
+    // Setting to enable/disable user rate limiting.
+    $settings->add(new admin_setting_configcheckbox(
+        'aiprovider_groq/enableuserratelimit',
+        new lang_string('enableuserratelimit', 'aiprovider_groq'),
+        new lang_string('enableuserratelimit_desc', 'aiprovider_groq'),
+        0,
+    ));
+
+    // Setting to set how many requests per hour are allowed for the user rate limit.
+    // Should only be enabled when user rate limiting is enabled.
+    $settings->add(new admin_setting_configtext(
+        'aiprovider_groq/userratelimit',
+        new lang_string('userratelimit', 'aiprovider_groq'),
+        new lang_string('userratelimit_desc', 'aiprovider_groq'),
+        10,
+        PARAM_INT,
+    ));
+    $settings->hide_if('aiprovider_groq/userratelimit', 'aiprovider_groq/enableuserratelimit', 'eq', 0);
+}
